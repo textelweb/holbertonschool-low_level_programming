ab056b13915cedca603122226d4e3cf307406d27
David Isaza <textelweb@gmail.com>
2022-06-18 02:45:50 +0000

    put

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * Descrription
- *
- */
+ *  * Description: main - Prints out a string to stdout.
+ *   * Return: 0 if success.
+ *     */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
